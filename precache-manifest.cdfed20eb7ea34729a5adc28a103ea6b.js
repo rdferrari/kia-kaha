a566fc7ecf14359729a4e36d956c5e4d3aef36ab
Rodrigo Ferrari <rodrigo@MacBook-Pro.local>
2020-03-31 10:55:07 +1300

    Updates

---
+++ precache-manifest.cdfed20eb7ea34729a5adc28a103ea6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8810b173da2ca9affe553f1a8bcdde",
+    "url": "/kia-kaha/index.html"
+  },
+  {
+    "revision": "777b31a5ac72a524826f",
+    "url": "/kia-kaha/static/css/main.cb587454.chunk.css"
+  },
+  {
+    "revision": "0cc05762877ab120c05b",
+    "url": "/kia-kaha/static/js/2.5f4a86f1.chunk.js"
+  },
+  {
+    "revision": "e64558e2f95ad6e9b52b3cecd9aea933",
+    "url": "/kia-kaha/static/js/2.5f4a86f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "777b31a5ac72a524826f",
+    "url": "/kia-kaha/static/js/main.f0f2a266.chunk.js"
+  },
+  {
+    "revision": "80c61f7843dd51ecbd7b",
+    "url": "/kia-kaha/static/js/runtime-main.528bce45.js"
+  }
+]);
